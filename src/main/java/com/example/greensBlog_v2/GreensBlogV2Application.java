a43fc5754f274f5aa219f53b37772a75d9f645a5
hand-jui <hand.jurajura@gmail.com>
2023-05-06 23:07:08 +0900

    초기 세팅

---
+++ src/main/java/com/example/greensBlog_v2/GreensBlogV2Application.java
@@ -0,0 +1,13 @@
+package com.example.greensBlog_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreensBlogV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreensBlogV2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/greensBlog_v2/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.example.greensBlog_v2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+	@GetMapping({ "/", "main" })
+	public String userPage(Model model) {
+
+		return "main";
+	}
+
+}
